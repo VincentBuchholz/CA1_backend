d7aa65875b05b0c3669d3e7ca9d8655962ebd8c2
Vincent Buchholz <vincbuchholz@gmail.com>
2022-03-09 10:10:39 +0100

    first commit

---
+++ src/main/java/facades/Populator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades;
+
+import javax.persistence.EntityManagerFactory;
+import utils.EMF_Creator;
+
+/**
+ *
+ * @author tha
+ */
+public class Populator {
+    public static void populate(){
+    }
+    
+    public static void main(String[] args) {
+        populate();
+    }
+}
---
+++ src/test/java/facades/FacadeTest.java
@@ -0,0 +1,47 @@
+package facades;
+
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+//Uncomment the line below, to temporarily disable this test
+//@Disabled
+public class FacadeTest {
+
+    private static EntityManagerFactory emf;
+    private static Facade facade;
+
+    public FacadeTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+       emf = EMF_Creator.createEntityManagerFactoryForTest();
+       facade = Facade.getFacadeExample(emf);
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+//        Clean up database after test is done or use a persistence unit with drop-and-create to start up clean on every test
+    }
+
+    // Setup the DataBase in a known state BEFORE EACH TEST
+    //TODO -- Make sure to change the code below to use YOUR OWN entity class
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+//        Remove any data after each test was run
+    }
+
+}
